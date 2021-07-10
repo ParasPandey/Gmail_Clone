ecf5caead3472ea784edbfbc9b8446079d7f1eba
Paras Pandey <69038847+ParasPandey@users.noreply.github.com>
2021-07-10 15:40:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyBLaG1HP2PGLVUvmZc037SCX9okS0JjOyE",
-  authDomain: "clone-5bbc1.firebaseapp.com",
-  databaseURL: "https://clone-5bbc1.firebaseio.com",
-  projectId: "clone-5bbc1",
-  storageBucket: "clone-5bbc1.appspot.com",
-  messagingSenderId: "1033278289754",
-  appId: "1:1033278289754:web:b7a26502b37d5ee3d8a563",
-  measurementId: "G-YXXPKS0YKE",
+//   your firebase config
 };
 
 // to initialize the firebase
